e3f6957180ebb65005978e1b7e0f5800641b75e9
cntjmj <ming@MingmatoMacBook-Air.local>
2016-01-11 15:01:55 +1100

    mobile-index-files, have to commit again

--- api/auth.php
+++ api/auth.php
@@ -9,10 +9,19 @@
 	} elseif (is_post()) {
 		$emailaddress = _post("emailaddress", "");
 		$password = _post("password", "");
-		
+
+		$fbEmail = _post("fbEmail", "");
+		$fbID = _post("fbID", "");
+		$fbName = _post("fbName", "");
+
 		try {
 			$auth = Auth::getInstance();
-			echo $auth->login($emailaddress, $password)->getJson();
+
+			if ($fbEmail != "") {
+				echo $auth->fbLogin($fbEmail, $fbID, $fbName)->getJson();
+			} else {
+				echo $auth->login($emailaddress, $password)->getJson();
+			}
 		} catch (Exception $e) {
 			$result = array("errCode" => $e->getCode(), "errMessage" => $e->getMessage());
 			echo json_encode($result);
